0d9ab3a7e8c412990ae26b194e0f7b2533e7ce17
Byron Wall <byron@byroni.us>
2024-05-11 00:03:22 -0400

    Filter meal plans

--- src/app/plan/page.tsx
+++ src/app/plan/page.tsx
@@ -8,13 +8,19 @@ export default async function PlanPage() {
 
   const plans = await api.recipe.getMealPlans();
 
+  // TODO: this filtering should be done on the server
+
   const plansThisMonth = plans.filter((plan) => {
+    // filter within -14 days and +14 days
+
     const date = new Date(plan.date);
     const now = new Date();
-    return (
-      date.getMonth() === now.getMonth() &&
-      date.getFullYear() === now.getFullYear()
-    );
+    const fourteenDaysAgo = new Date();
+    fourteenDaysAgo.setDate(now.getDate() - 14);
+    const fourteenDaysFromNow = new Date();
+    fourteenDaysFromNow.setDate(now.getDate() + 14);
+
+    return date >= fourteenDaysAgo && date <= fourteenDaysFromNow;
   });
 
   return (
@@ -24,13 +30,6 @@ export default async function PlanPage() {
       <H2>Plans this month</H2>
       <div className="flex flex-wrap gap-2">
         {plansThisMonth.map((plan) => (
-          <PlanCard key={plan.id} plan={plan} />
-        ))}
-      </div>
-
-      <H2>All Meal Plans</H2>
-      <div className="">
-        {plans.map((plan) => (
           <PlanCard key={plan.id} plan={plan} />
         ))}
       </div>
