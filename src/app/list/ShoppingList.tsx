a8facd2ca6fc8d21a5df7ea0b6acbf6d0063d8dc
Byron Wall <byron@byroni.us>
2024-05-12 22:31:48 -0400

    Add shopping list groups Recipe and aisle Give toggles to show and hide sections

--- src/app/list/ShoppingList.tsx
+++ src/app/list/ShoppingList.tsx
@@ -5,6 +5,14 @@ import { H2 } from "~/components/ui/typography";
 import { ShoppingListCard } from "./ShoppingListCard";
 import { api } from "~/trpc/react";
 import { ShoppingRecipeItem } from "./ShoppingRecipeItem";
+import { useState } from "react";
+import { RadioGroup, RadioGroupItem } from "~/components/ui/radio-group";
+import { Label } from "~/components/ui/label";
+import { Switch } from "~/components/ui/switch";
+
+const groupModes = ["recipe", "aisle"] as const;
+
+type GroupMode = (typeof groupModes)[number];
 
 export function ShoppingList() {
   const { data: shoppingList = [] } =
@@ -21,6 +29,33 @@ export function ShoppingList() {
     }
   }
 
+  const [groupMode, setGroupMode] = useState<GroupMode>("recipe");
+
+  const groupedShoppingList = shoppingList.reduce(
+    (acc, item) => {
+      if (groupMode === "recipe" && item.Recipe) {
+        const recipeName = recipeNameById[item.Recipe.id]!;
+        if (acc[recipeName] === undefined) {
+          acc[recipeName] = [];
+        }
+        acc[recipeName]!.push(item);
+      } else if (groupMode === "aisle") {
+        const aisle = item.ingredient?.aisle || "Unknown Aisle";
+        if (acc[aisle] === undefined) {
+          acc[aisle] = [];
+        }
+        acc[aisle]!.push(item);
+      }
+      return acc;
+    },
+    {} as Record<string, typeof shoppingList>,
+  );
+
+  const [hiddenKeys, setHiddenKeys] = useState<string[]>([]);
+
+  const groupedKeys = Object.keys(groupedShoppingList);
+  groupedKeys.sort();
+
   return (
     <>
       <H2>Recipes included</H2>
@@ -29,12 +64,59 @@ export function ShoppingList() {
           <ShoppingRecipeItem key={id} id={id} name={name} />
         ))}
       </div>
-
-      <H2>List</H2>
+      <H2>Group Mode</H2>
       <div>
-        {shoppingList.map((item) => (
-          <ShoppingListCard key={item.id} item={item} />
-        ))}
+        <RadioGroup
+          defaultValue={groupMode}
+          onValueChange={(value) => setGroupMode(value as GroupMode)}
+        >
+          {groupModes.map((mode) => (
+            <div key={mode} className="flex items-center space-x-2">
+              <RadioGroupItem value={mode} />
+              <Label>{mode}</Label>
+            </div>
+          ))}
+        </RadioGroup>
+      </div>
+      <H2>Grouped List</H2>
+      <div>
+        {groupedKeys.map((key) => {
+          const items = groupedShoppingList[key]!;
+          const isVisible = !hiddenKeys.includes(key);
+
+          return (
+            <div key={key}>
+              <div className="flex items-center gap-2">
+                <Switch
+                  checked={isVisible}
+                  onCheckedChange={(checked) =>
+                    setHiddenKeys((keys) => {
+                      if (keys.includes(key)) {
+                        return keys.filter((k) => k !== key);
+                      } else {
+                        return [...keys, key];
+                      }
+                    })
+                  }
+                  id={`group-${key}`}
+                />
+                <Label
+                  htmlFor={`group-${key}`}
+                  className="cursor-pointer select-none"
+                >
+                  <H2>{key}</H2>
+                </Label>
+              </div>
+              {isVisible ? (
+                <div>
+                  {items.map((item) => (
+                    <ShoppingListCard key={item.id} item={item} />
+                  ))}
+                </div>
+              ) : null}
+            </div>
+          );
+        })}
       </div>
     </>
   );
