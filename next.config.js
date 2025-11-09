fd4f4423d62627c26efc2c9615428b9ffdaef0c3
Byron Wall <byron@byroni.us>
2025-11-09 10:03:26 -0500

    Revise next config to ignore bad env during build

--- next.config.js
+++ next.config.js
@@ -8,7 +8,15 @@ const envFile = await import("./src/env.js");
 // ENV Looks like: NEXT_PUBLIC_MEDIA_BASE_URL=http://localhost:9000/recipes-media
 const mediaBaseUrl = envFile.env.MEDIA_BASE_URL;
 if (!mediaBaseUrl) {
-  throw new Error("APP_HOST is not set");
+  if (process.env.IS_BUILD) {
+    console.warn(
+      "MEDIA_BASE_URL is not set.  Skipping error because IS_BUILD is set.",
+    );
+  } else {
+    throw new Error(
+      "MEDIA_BASE_URL is not set.  Throwing error because IS_BUILD is not set.",
+    );
+  }
 }
 const mediaAsUri = new URL(mediaBaseUrl);
 const protocol = mediaAsUri.protocol.replace(":", "");
